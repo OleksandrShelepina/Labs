4fbfcb54bff1bb1f416a1a09a1b68a97c673ce58
OleksandrShelepina <voroncroe@gmail.com>
2024-11-21 12:03:09 +0200

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,25 +1,18 @@
 def remove_duplicates(input_list):
-    """
-    Видаляє повторення зі списку.
-    """
-    return list(set(input_list))
+    # Зберігаємо порядок елементів, видаляючи дублікати
+    seen = set()
+    return [x for x in input_list if not (x in seen or seen.add(x))]
 
 def sort_custom(input_list):
-    """
-    Сортує список: числа за зростанням, рядки в алфавітному порядку.
-    """
-    numbers = sorted([item for item in input_list if isinstance(item, (int, float))])
-    strings = sorted([item for item in input_list if isinstance(item, str)])
+    # Розділяємо числа і рядки
+    numbers = sorted([x for x in input_list if isinstance(x, (int, float))])
+    strings = sorted([x for x in input_list if isinstance(x, str)])
     return numbers + strings
-
 
 original_list = [3, 1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'анаконда']
 
-
-unique_list = remove_duplicates(original_list)
-sorted_list = sort_custom(unique_list)
+unique_list = remove_duplicates(original_list)  # Видаляємо дублікати
+sorted_list = sort_custom(unique_list)  # Сортуємо список
 
 print("Список без повторень:", unique_list)
 print("Відсортований список:", sorted_list)
-
-
